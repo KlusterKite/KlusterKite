55ecf4003d1dc2af03c8305d29e10989c269d892
Mikhail Kantarovskiy <kantoramob@gmail.com>
2015-08-04 17:57:29 +0300

    Service starter project base

---
+++ TaxiKit.Core/TaxiKit.Core.Service/Bootstrapper.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Bootstrapper.cs" company="TaxiKit">
+//   All rights reserved
+// </copyright>
+// <summary>
+//   Dependency injection configuration
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TaxiKit.Core.Service
+{
+    using System.Configuration;
+
+    using Akka.Actor;
+    using Akka.Configuration.Hocon;
+
+    using Castle.Facilities.TypedFactory;
+    using Castle.MicroKernel.Registration;
+    using Castle.MicroKernel.Resolvers.SpecializedResolvers;
+    using Castle.Windsor;
+
+    using CommonServiceLocator.WindsorAdapter;
+
+    using Microsoft.Practices.ServiceLocation;
+
+    /// <summary>
+    /// Dependency injection configuration
+    /// </summary>
+    public class Bootstrapper
+    {
+        /// <summary>
+        ///  Dependency injection configuration
+        /// </summary>
+        /// <param name="container">Dependency injection container</param>
+        public static void Configure(IWindsorContainer container)
+        {
+            container.AddFacility<TypedFactoryFacility>();
+            container.Kernel.Resolver.AddSubResolver(new ArrayResolver(container.Kernel, true));
+            container.Register(Component.For<Controller>().LifestyleTransient());
+
+            var section = (AkkaConfigurationSection)ConfigurationManager.GetSection("akka");
+            var config = section.AkkaConfig;
+            var actorSystem = ActorSystem.Create("TaxiKit", config);
+            container.Register(Component.For<ActorSystem>().Instance(actorSystem).LifestyleSingleton());
+            ServiceLocator.SetLocatorProvider(() => new WindsorServiceLocator(container));
+        }
+    }
+}
---
+++ TaxiKit.Core/TaxiKit.Core.Service/Controller.cs
@@ -0,0 +1,82 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Controller.cs" company="TaxiKit">
+//   All rights reserved
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TaxiKit.Core.Service
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Akka.Actor;
+    using Akka.Event;
+
+    using Castle.MicroKernel;
+    using Castle.Windsor;
+
+    /// <summary>
+    /// Service controller
+    /// </summary>
+    public class Controller
+    {
+        /// <summary>
+        /// The actor system
+        /// </summary>
+        private readonly ActorSystem actorSystem;
+
+        /// <summary>
+        /// Service start cancellation token (in case of service stop, before service start was actualy completed)
+        /// </summary>
+        private readonly CancellationTokenSource startCancellationToken = new CancellationTokenSource();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Controller"/> class.
+        /// </summary>
+        /// <param name="actorSystem">
+        /// The actor system.
+        /// </param>
+        public Controller(ActorSystem actorSystem)
+        {
+            this.actorSystem = actorSystem;
+        }
+
+        /// <summary>
+        /// Main system logger
+        /// </summary>
+        private ILoggingAdapter Logger => this.actorSystem.Log;
+
+        /// <summary>
+        /// Запуск сервиса
+        /// </summary>
+        /// <param name="container">
+        /// The container.
+        /// </param>
+        /// <returns>
+        /// Успех запуска
+        /// </returns>
+        public bool Start(IWindsorContainer container)
+        {
+            Task.Factory.StartNew(
+                () =>
+                    {
+                        Application.Start(this.actorSystem, container);
+                        this.Logger.Info("Service started.");
+                    },
+                this.startCancellationToken.Token);
+            this.Logger.Info("Service start initiated.");
+            return true;
+        }
+
+        /// <summary>
+        /// Остановка сервиса
+        /// </summary>
+        public void Stop()
+        {
+            this.startCancellationToken.Cancel(false);
+            this.actorSystem.Shutdown();
+            Application.Stop();
+            this.Logger.Info("Service was stopped.");
+        }
+    }
+}
