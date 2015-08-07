82cc01cca9b5da8c1cd503c0f7ba06c68cc29eb5
Mikhail Kantarovskiy <kantoramob@gmail.com>
2015-08-07 18:59:34 +0300

    Redis connection and beginning of redis moq in tests

--- TaxiKit.Core/TaxiKit.Core.Service/Bootstrapper.cs
+++ TaxiKit.Core/TaxiKit.Core.Service/Bootstrapper.cs
@@ -23,6 +23,8 @@ namespace TaxiKit.Core.Service
 
     using Microsoft.Practices.ServiceLocation;
 
+    using StackExchange.Redis;
+
     /// <summary>
     /// Dependency injection configuration
     /// </summary>
@@ -38,6 +40,14 @@ namespace TaxiKit.Core.Service
             container.Kernel.Resolver.AddSubResolver(new ArrayResolver(container.Kernel, true));
             container.Register(Component.For<Controller>().LifestyleTransient());
 
+            // registering redis connection
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(ConfigurationManager.ConnectionStrings["redis"].ConnectionString);
+            container.Register(
+                Component
+                .For<IDatabase>()
+                .UsingFactoryMethod(() => redis.GetDatabase()).LifestyleScoped());
+
+            // starting akka system
             var section = (AkkaConfigurationSection)ConfigurationManager.GetSection("akka");
             var config = section.AkkaConfig;
             var actorSystem = ActorSystem.Create("TaxiKit", config);
