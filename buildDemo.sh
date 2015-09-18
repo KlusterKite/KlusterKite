e6a428430f3cebf48e3eead5f233e014ce46b980
Mikhail Kantarovskiy <kantoramob@gmail.com>
2015-09-18 19:00:33 +0300

    Some more renaming

--- buildDemo.sh
+++ buildDemo.sh
@@ -3,22 +3,22 @@ echo "Stopping"
 killall mono
 
 echo "Building"
-xbuild /noconsolelogger /p:TargetFrameworkVersion="v4.5" TaxiKit.Core/TaxiKit.Core.sln
+xbuild /noconsolelogger /p:TargetFrameworkVersion="v4.5" ClusterKit.Core/ClusterKit.Core.sln
 
 echo "Deploing"
-rm -rf ~/TaxiKitDemo
+rm -rf ~/ClusterKitDemo
 
-mkdir -p ~/TaxiKitDemo/SeedNode1
-cp  TaxiKit.Core/TaxiKit.Core.Service/bin/Debug/* ~/TaxiKitDemo/SeedNode1
-sed -i 's/port = 0/port = 2551/g' ~/TaxiKitDemo/SeedNode1/akka.config
-sed -i 's/seed-nodes = \[\]/seed-nodes = \["akka.tcp:\/\/TaxiKit@127.0.0.1:2551", "akka.tcp:\/\/TaxiKit@127.0.0.1:2552"\]/g' ~/TaxiKitDemo/SeedNode1/akka.config
+mkdir -p ~/ClusterKitDemo/SeedNode1
+cp  ClusterKit.Core/ClusterKit.Core.Service/bin/Debug/* ~/ClusterKitDemo/SeedNode1
+sed -i 's/port = 0/port = 2551/g' ~/ClusterKitDemo/SeedNode1/akka.config
+sed -i 's/seed-nodes = \[\]/seed-nodes = \["akka.tcp:\/\/ClusterKit@127.0.0.1:2551", "akka.tcp:\/\/ClusterKit@127.0.0.1:2552"\]/g' ~/ClusterKitDemo/SeedNode1/akka.config
 
-mkdir -p ~/TaxiKitDemo/SeedNode2
-cp  TaxiKit.Core/TaxiKit.Core.Service/bin/Debug/* ~/TaxiKitDemo/SeedNode2
-sed -i 's/port = 0/port = 2552/g' ~/TaxiKitDemo/SeedNode2/akka.config
-sed -i 's/seed-nodes = \[\]/seed-nodes = \["akka.tcp:\/\/TaxiKit@127.0.0.1:2551", "akka.tcp:\/\/TaxiKit@127.0.0.1:2552"\]/g' ~/TaxiKitDemo/SeedNode2/akka.config
+mkdir -p ~/ClusterKitDemo/SeedNode2
+cp  ClusterKit.Core/ClusterKit.Core.Service/bin/Debug/* ~/ClusterKitDemo/SeedNode2
+sed -i 's/port = 0/port = 2552/g' ~/ClusterKitDemo/SeedNode2/akka.config
+sed -i 's/seed-nodes = \[\]/seed-nodes = \["akka.tcp:\/\/ClusterKit@127.0.0.1:2551", "akka.tcp:\/\/ClusterKit@127.0.0.1:2552"\]/g' ~/ClusterKitDemo/SeedNode2/akka.config
 
-mkdir -p ~/TaxiKitDemo/Node1
-cp  TaxiKit.Core/TaxiKit.Core.Service/bin/Debug/* ~/TaxiKitDemo/Node1
-sed -i 's/seed-nodes = \[\]/seed-nodes = \["akka.tcp:\/\/TaxiKit@127.0.0.1:2551", "akka.tcp:\/\/TaxiKit@127.0.0.1:2552"\]/g' ~/TaxiKitDemo/Node1/akka.config
+mkdir -p ~/ClusterKitDemo/Node1
+cp  ClusterKit.Core/ClusterKit.Core.Service/bin/Debug/* ~/ClusterKitDemo/Node1
+sed -i 's/seed-nodes = \[\]/seed-nodes = \["akka.tcp:\/\/ClusterKit@127.0.0.1:2551", "akka.tcp:\/\/ClusterKit@127.0.0.1:2552"\]/g' ~/ClusterKitDemo/Node1/akka.config
 
--- runDemo.sh
+++ runDemo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 killall mono
-mono-service -d:/home/kantora/TaxiKitDemo/SeedNode1/ -l:/tmp/SeedNode1.lck /home/kantora/TaxiKitDemo/SeedNode2/TaxiKit.Core.Service.exe
-mono-service -d:/home/kantora/TaxiKitDemo/SeedNode1/ -l:/tmp/SeedNode1.lck /home/kantora/TaxiKitDemo/SeedNode2/TaxiKit.Core.Service.exe
+mono-service -d:/home/kantora/ClusterKitDemo/SeedNode1/ -l:/tmp/SeedNode1.lck /home/kantora/ClusterKitDemo/SeedNode2/ClusterKit.Core.Service.exe
+mono-service -d:/home/kantora/ClusterKitDemo/SeedNode1/ -l:/tmp/SeedNode1.lck /home/kantora/ClusterKitDemo/SeedNode2/ClusterKit.Core.Service.exe
