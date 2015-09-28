b102399c6cf7013bb8e2ddbd367314b56517b51d
Mikhail Kantarovskiy <kantoramob@gmail.com>
2015-09-28 11:40:50 +0300

    Service will read akka.hocon as main configuration

---
+++ ClusterKit.Monitoring/ClusterKit.Monitoring/Actors/WatcherActor.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WatcherActor.cs" company="ClusterKit">
+//   All rights reserved
+// </copyright>
+// <summary>
+//   Watcher actor. It's main purpose to monitor any cluster changes and store complete
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ClusterKit.Monitoring.Actors
+{
+    using Akka.Actor;
+
+    /// <summary>
+    /// Watcher actor. It's main purpose to monitor any cluster changes and store complete data about current cluster health
+    /// </summary>
+    public class WatcherActor : ReceiveActor
+    {
+    }
+}
---
+++ ClusterKit.Monitoring/ClusterKit.Monitoring/WebApi/MonitoringController.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MonitoringController.cs" company="ClusterKit">
+//   All rights reserved
+// </copyright>
+// <summary>
+//   Manages web requests to describe current cluster health state
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ClusterKit.Monitoring.WebApi
+{
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// Manages web requests to describe current cluster health state
+    /// </summary>
+    public class MonitoringController : Controller
+    {
+    }
+}
