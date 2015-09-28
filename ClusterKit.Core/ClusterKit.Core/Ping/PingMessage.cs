180ea4a5745b9762b01a8283c3c7110a838c0660
Mikhail Kantarovskiy <kantoramob@gmail.com>
2015-09-28 10:20:53 +0300

    Introducing PingActor

---
+++ ClusterKit.Core/ClusterKit.Core/Ping/PingMessage.cs
@@ -0,0 +1,18 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PingMessage.cs" company="ClusterKit">
+//   All rights reserved
+// </copyright>
+// <summary>
+//   Standard request message for <seealso cref="PingActor" />
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ClusterKit.Core.Ping
+{
+    /// <summary>
+    /// Standard request message for <seealso cref="PingActor"/>
+    /// </summary>
+    public class PingMessage
+    {
+    }
+}
---
+++ ClusterKit.Core/ClusterKit.Core/Ping/PongMessage.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PongMessage.cs" company="ClusterKit">
+//   All rights reserved
+// </copyright>
+// <summary>
+//   Standard response message for <seealso cref="PingActor" />
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ClusterKit.Core.Ping
+{
+    /// <summary>
+    /// Standard response message for <seealso cref="PingActor"/>
+    /// </summary>
+    public class PongMessage
+    {
+    }
+}
