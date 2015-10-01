809358f6f697f60bccadfb9c9fcdae542dba3a42
Mikhail Kantarovskiy <kantoramob@gmail.com>
2015-10-01 16:39:50 +0300

    Monitoring started using SignalR for web communication

--- ClusterKit.Monitoring/ClusterKit.Monitoring.Web/Scripts/ClusterKit.Monitoring.js
+++ ClusterKit.Monitoring/ClusterKit.Monitoring.Web/Scripts/ClusterKit.Monitoring.js
@@ -1,9 +1,31 @@
 ﻿(function ($) {
+    var signalrConnection;
+    var hubProxy;
+
     var clusterState = {
         Connected: ko.observable(false)
     };
 
     $(document).ready(function () {
         ko.applyBindings(clusterState, $("#clusterMonitoringView")[0]);
+        signalrConnection = $.hubConnection();
+        hubProxy = signalrConnection.createHubProxy("monitoring");
+
+        // todo: register events
+        signalrConnection.reconnected(function () {
+            clusterState.Connected(true);
+        });
+
+        signalrConnection.reconnecting(function () {
+            clusterState.Connected(false);
+        });
+
+        signalrConnection.disconnected(function () {
+            clusterState.Connected(false);
+        });
+
+        signalrConnection.start()
+            .done(function () { clusterState.Connected(true) })
+            .fail(function () { clusterState.Connected(false) });
     });
 })(jQuery);
---
+++ ClusterKit.Monitoring/ClusterKit.Monitoring/MonitoringHub.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MonitoringHub.cs" company="ClusterKit">
+//   All rights reserved
+// </copyright>
+// <summary>
+//   Hub for signalR communication
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ClusterKit.Monitoring
+{
+    using Microsoft.AspNet.SignalR;
+
+    /// <summary>
+    /// Hub for signalR communication
+    /// </summary>
+    public class MonitoringHub : Hub
+    {
+    }
+}
