480ce99facbd33460d83af143552af15fa349aea
Sergey Purikov <purikov@gmail.com>
2016-04-11 16:22:53 +0300

    Form fields cosmetic changes
    New attribute allows changing of input fields length.

--- Docker/ClusterKitMonitoring/web/src/components/Forms/MultipleInput.js
+++ Docker/ClusterKitMonitoring/web/src/components/Forms/MultipleInput.js
@@ -3,9 +3,31 @@ import React, { Component, PropTypes } from 'react';
 export default class MultipleInput extends Component {
   static propTypes = {
     field: PropTypes.arrayOf(PropTypes.object),
-    label: PropTypes.string.isRequired
+    label: PropTypes.string.isRequired,
+    size: PropTypes.string
   }
 
+  /**
+   * Get the className for selected size of the form field
+   */
+  getSizeClassName = () => {
+    const {size} = this.props;
+
+    switch (size) {
+      case 'small':
+        return ' col-xs-2 ';
+
+      case 'medium':
+        return ' col-xs-4 ';
+
+      default:
+        return ' col-xs-8 ';
+    }
+  }
+
+  /**
+   * Process adding new input field to the form
+   */
   handleAdd = () => {
     const {field} = this.props;
 
@@ -15,16 +37,19 @@ export default class MultipleInput extends Component {
 
   render() {
     const {field, label} = this.props;
+    const classSize = this.getSizeClassName();
 
     return (
       <div className="row">
-        <label>{label} <i className="fa fa-plus-circle" role="button" onClick={this.handleAdd}></i></label>
-        {!field.length && <div>No Items</div>}
-        {field && field.map((item, index) =>
-            <div key={index} className="form-group">
+        <div className={classSize}>
+          <label>{label} <i className="fa fa-plus-circle" role="button" onClick={this.handleAdd}></i></label>
+          {!field.length && <div>No Items</div>}
+          {field && field.map((item, index) =>
+            <div key={index} className={'form-group'}>
               <input type="text" className="form-control" id={item.name} {...item}/>
             </div>
           )}
+        </div>
       </div>
     );
   }
