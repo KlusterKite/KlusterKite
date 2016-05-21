e3bcee17a860535eb514ab3cb67eb45cbd44e062
Denis Kim <denis@kim.aero>
2016-05-21 09:44:18 +0000

    Added export with mono threads limitations

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+export MONO_THREADS_PER_CPU=2048
 mono --runtime=v4.0 nuget.exe "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
 mono --runtime=v4.0 packages/FAKE/tools/FAKE.exe build.fsx
