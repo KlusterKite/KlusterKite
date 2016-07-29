5d367feb8347f1ef3f28dfff34e5d9c1256d9434
Michael Kantarovsky <kantoramob@gmail.com>
2016-07-29 09:18:45 +0300

    Akka update

---
+++ ClusterKit.Core/ClusterKit.Core/Tools/CurrentLocale.cs
@@ -0,0 +1,42 @@
+﻿namespace ClusterKit.Core.Tools
+{
+    using System;
+    using System.Globalization;
+    using System.Threading;
+
+    using JetBrains.Annotations;
+
+    /// <summary>
+    /// Helper class to set current thread culture. Will restore original culture on dispose.
+    /// </summary>
+    public class CurrentLocale : IDisposable
+    {
+        private readonly Thread localThread;
+        private readonly CultureInfo originalCulture;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CurrentLocale"/> class.
+        /// </summary>
+        /// <param name="locale">Locale code to set</param>
+        public CurrentLocale([NotNull] string locale)
+        {
+            this.localThread = Thread.CurrentThread;
+            this.originalCulture = this.localThread.CurrentUICulture;
+
+            try
+            {
+                this.localThread.CurrentUICulture = CultureInfo.GetCultureInfo(locale);
+            }
+            catch (CultureNotFoundException)
+            {
+                this.localThread.CurrentUICulture = CultureInfo.GetCultureInfo("en-US");
+            }
+        }
+
+        /// <summary>Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.</summary>
+        public void Dispose()
+        {
+            this.localThread.CurrentUICulture = this.originalCulture;
+        }
+    }
+}
--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
-NuGet.exe "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
+IF NOT EXIST packages\FAKE\tools\FAKE.exe (
+	nuget.exe "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"                        
+)
+
 packages\FAKE\tools\FAKE.exe build.fsx %1
