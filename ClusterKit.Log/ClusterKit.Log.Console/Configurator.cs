f3caacbda57497fe8ec2601359e07db891496be0
Michael Kantarovsky <kantoramob@gmail.com>
2016-08-24 13:25:27 +0300

    Log modules

---
+++ ClusterKit.Log/ClusterKit.Log.Console/Configurator.cs
@@ -0,0 +1,39 @@
+﻿namespace ClusterKit.Log.Console
+{
+    using System;
+
+    using Akka.Configuration;
+
+    using ClusterKit.Core;
+
+    using JetBrains.Annotations;
+
+    using Serilog;
+    using Serilog.Events;
+
+    /// <summary>
+    /// Colored consoled configuration
+    /// </summary>
+    [UsedImplicitly]
+    public class Configurator : ILoggerConfigurator
+    {
+        /// <summary>
+        /// Perfoms configuration
+        /// </summary>
+        /// <param name="configuration">Previous configuration</param>
+        /// <param name="config">Akka configuration</param>
+        /// <returns>Updated configuration</returns>
+        public LoggerConfiguration Configure(LoggerConfiguration configuration, Config config)
+        {
+            var minimumLevel = config.GetString("ClusterKit.Log.Console.minimumLevel", "none")?.Trim();
+
+            LogEventLevel level;
+            if (!Enum.TryParse(minimumLevel, true, out level))
+            {
+                return configuration;
+            }
+
+            return configuration.WriteTo.ColoredConsole(LogEventLevel.Debug);
+        }
+    }
+}
---
+++ ClusterKit.Log/ClusterKit.Log.ElasticSearch/Configurator.cs
@@ -0,0 +1,66 @@
+﻿namespace ClusterKit.Log.ElasticSearch
+{
+    using System;
+    using System.Linq;
+
+    using Akka.Configuration;
+
+    using ClusterKit.Core;
+
+    using JetBrains.Annotations;
+
+    using Serilog;
+    using Serilog.Debugging;
+    using Serilog.Events;
+    using Serilog.Sinks.Elasticsearch;
+
+    /// <summary>
+    /// Elastic search configuration
+    /// </summary>
+    [UsedImplicitly]
+    public class Configurator : ILoggerConfigurator
+    {
+        /// <summary>
+        /// Perfoms configuration
+        /// </summary>
+        /// <param name="configuration">Previous configuration</param>
+        /// <param name="config">Akka configuration</param>
+        /// <returns>Updated configuration</returns>
+        public LoggerConfiguration Configure(LoggerConfiguration configuration, Config config)
+        {
+            var minimumLevel = config.GetString("ClusterKit.Log.ElasticSearch.minimumLevel", "none")?.Trim();
+
+            LogEventLevel level;
+            if (!Enum.TryParse(minimumLevel, true, out level))
+            {
+                return configuration;
+            }
+
+            var nodes = config.GetStringList("ClusterKit.Log.ElasticSearch.nodes");
+
+            var indexFormat = config.GetString("ClusterKit.Log.ElasticSearch.indexFormat", "clusterkit-{0:yyyy.MM.dd}").ToLower();
+
+            Log.Information(
+                "{Type}: \n\tMinimum level: {MinimumLevel}\n\tIndex format: {IndexFormat}\n\tNodes:\n\t\t{NodeList}\n",
+                this.GetType().FullName,
+                minimumLevel,
+                indexFormat,
+                string.Join("\n\t\t", nodes));
+
+
+            SelfLog.Enable(Console.WriteLine);
+            var options = new ElasticsearchSinkOptions(nodes.Select(s => new Uri(s)))
+                              {
+                                  MinimumLogEventLevel = level,
+                                  AutoRegisterTemplate = true,
+                                  IndexFormat = indexFormat,
+                                  //CustomFormatter = new ExceptionAsJsonObjectFormatter(renderMessage: true)
+
+                              };
+
+            return configuration.WriteTo.Elasticsearch(options);
+
+
+        }
+    }
+}
