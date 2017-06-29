421faf0e9fbbdc9b774d5daff1009302fbacc019
Mikhail Kantarovskiy <kantoramob@gmail.com>
2017-06-29 14:32:05 +0300

    NodeUpgradeDowngrade: build script decomposition

---
+++ build.config.fsx
@@ -0,0 +1,16 @@
+#r ".fake/build.fsx/packages/netcorebuild/Fake.Core.Environment/lib/netstandard1.6/Fake.Core.Environment.dll"
+namespace ClusterKit.Build
+
+open System.IO
+open Fake.Core.Environment
+
+module Config =
+
+    let testPackageName = "ClusterKit.Core"
+    let buildDir = Path.GetFullPath("./build")
+    let mutable packageDir = Path.GetFullPath("./packageOut")
+    let packagePushDir = Path.GetFullPath("./packagePush")
+    let packageThirdPartyDir = Path.GetFullPath("./packageThirdPartyDir")
+
+    let envVersion = environVarOrDefault "version" null
+    let mutable version = if envVersion <> null then envVersion else "0.0.0-local"
