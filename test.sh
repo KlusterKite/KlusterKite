529e6a7077e1f9d7ce54af61c243f683b012fe86
Mikhail Kantarovskiy <kantoramob@gmail.com>
2017-07-05 17:41:12 +0300

    NodeUpgradeDowngrade: cleanup

--- test.sh
+++
@@ -1,2 +0,0 @@
-mono --runtime=v4.0 nuget.exe "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
-mono --runtime=v4.0 packages/FAKE/tools/FAKE.exe build.fsx Test -ev version 0.0.0.0-alpha
